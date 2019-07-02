918fce3da920a77876f1ed0a79f92c530f2c8eb3
Joseph Delle Donne <j.delledonne4@gmail.com>
2019-07-02 12:40:12 +0200

    Edit test file

--- scripts/mpgTest.R
+++ scripts/mpgTest.R
@@ -1,6 +1,9 @@
 # Author: Joe Delle Donne
+# Date: 07/02/2019
 
 ggplot(data = mpg) + 
     geom_point(mapping = aes(x = displ, y = hwy, color = class)) +
-    geom_smooth(mapping = aes(x = displ, y = hwy))
+    geom_smooth(mapping = aes(x = displ, y = hwy)) +
+    ggtitle("mpg data test")
+
 
